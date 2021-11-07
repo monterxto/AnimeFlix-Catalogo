ae402e1ba464bbb5dc13ce85f83ce43f59483ee6
Antonio Carlos <toniko1910@gmail.com>
2021-11-07 14:19:59 -0300

    Inicializando serviço de categoria

---
+++ src/app.module.ts
@@ -0,0 +1,19 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { CategoriesModule } from './categories/categories.module';
+
+@Module({
+  imports: [
+    MongooseModule.forRoot('mongodb://db:27017/local', {
+      user: "root",
+      pass: "nest",
+      authSource: 'admin'
+    }),
+    CategoriesModule
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ src/categories/entities/category.entity.ts
@@ -0,0 +1,21 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type CategoryDocument = Category & Document;
+
+@Schema()
+export class Category {
+  @Prop()
+  id: string;
+
+  @Prop()
+  name: string;
+
+  @Prop()
+  description: string;
+  
+  @Prop()
+  isActive: string;
+}
+
+export const CategorySchema = SchemaFactory.createForClass(Category);
