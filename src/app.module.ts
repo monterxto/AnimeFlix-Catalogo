1fe0c68b3b6720b1851021e570baf8f30d289808
Antonio Carlos <toniko1910@gmail.com>
2021-11-07 22:16:23 -0300

    feature() criar serviço de atualizar categoria

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { CategoriesModule } from './categories/categories.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://db/local', {
-      user: "root",
-      pass: "nest",
-      authSource: 'admin'
+      user: process.env.USER_DB,
+      pass: process.env.PASS_DB,
+      authSource: 'admin',
     }),
-    CategoriesModule
+    CategoriesModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
--- src/categories/categories.service.ts
+++ src/categories/categories.service.ts
@@ -6,10 +6,7 @@ import { CategoriesRepository } from './repository/categories.repository';
 
 @Injectable()
 export class CategoriesService {
-
-  constructor(
-    private repository: CategoriesRepository
-  ) {}
+  constructor(private repository: CategoriesRepository) {}
 
   public async create(createCategoryDto: CreateCategoryDto) {
     return await this.repository.create(createCategoryDto);
@@ -20,21 +17,25 @@ export class CategoriesService {
   }
 
   async findOneById(id: string) {
-    let _id: Types.ObjectId = new Types.ObjectId(id)
+    let _id: Types.ObjectId = new Types.ObjectId(id);
     return await this.repository.findOneById(_id);
   }
 
-  update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return `This action updates a #${id} category`;
+  async updateById(id: string, updateCategoryDto: UpdateCategoryDto) {
+    let _id: Types.ObjectId = new Types.ObjectId(id);
+    let updated = await this.repository.updateById(_id, updateCategoryDto);
+
+    if (updated.modifiedCount > 0) return true;
+
+    return false;
   }
 
   public async removeOneById(id: string) {
-    let _id: Types.ObjectId = new Types.ObjectId(id)
+    let _id: Types.ObjectId = new Types.ObjectId(id);
     let removed = await this.repository.removeOneById(_id);
-    
-    if (removed.modifiedCount > 0)
-      return true
 
-    return false
+    if (removed.modifiedCount > 0) return true;
+
+    return false;
   }
 }
